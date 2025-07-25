7ba49b3afd99ccb662858b6cfa0fcedea288bec3
Hebrain <hebrasantos@gmail.com>
2025-07-25 18:52:49 -0300

    add EntityFrameworkCore e mudança na estrutura para usar o memoreCacheadd EntityFrameworkCore e mudança na estrutura para usar o memoreCache

--- Api-Pedido/Config/ServiceCollectionExtensions.cs
+++ Api-Pedido/Config/ServiceCollectionExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Application.Features.CriarPedido;
 using Application.Interfaces;
+using Infraestrutura.Context;
 using Infraestrutura.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api_Pedido.Config
 {
@@ -10,6 +13,7 @@ namespace Api_Pedido.Config
         {
             services.AddScoped<IPedidoRepository, PedidoRepository>();
             services.AddMediatR(m => m.RegisterServicesFromAssembly(typeof(CriarPedidoHandler).Assembly));
+            services.AddDbContext<PedidoContext>(o => o.UseInMemoryDatabase("PedidoDB"));
 
             return services;
         }
---
+++ Infraestrutura/Context/PedidoContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestrutura.Context
+{
+    public class PedidoContext(DbContextOptions<PedidoContext> options) : DbContext(options)
+    {
+        public DbSet<Pedido> Pedido => Set<Pedido>();
+    }
+}
